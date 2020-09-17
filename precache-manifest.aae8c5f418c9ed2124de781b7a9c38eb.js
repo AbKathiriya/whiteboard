ad96516485d0b317e83f4f8e1d1ee0ed48cbda51
Akash Kathiriya <akash.kathiriya@gmail.com>
2020-09-17 14:59:18 +0530

    Updates

---
+++ precache-manifest.aae8c5f418c9ed2124de781b7a9c38eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb224648dc642747f5f24947218737f",
+    "url": "/whiteboard/index.html"
+  },
+  {
+    "revision": "22fe9f50f24bd7470896",
+    "url": "/whiteboard/static/css/main.c5ad6370.chunk.css"
+  },
+  {
+    "revision": "b0751f9b0323d44a2fa1",
+    "url": "/whiteboard/static/js/2.9b37b473.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whiteboard/static/js/2.9b37b473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22fe9f50f24bd7470896",
+    "url": "/whiteboard/static/js/main.db382c33.chunk.js"
+  },
+  {
+    "revision": "95f52f808f7b1ca668e1",
+    "url": "/whiteboard/static/js/runtime-main.c2696591.js"
+  }
+]);
